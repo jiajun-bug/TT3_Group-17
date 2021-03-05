c90bbd76246fb4af345f5728aff6d664e9e4f058
Hariz1995 <harizyousuf@gmail.com>
2021-03-05 16:28:23 +0800

    fixed addtransact api

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
 import React from 'react';
-import Login from './Login';
+// import Login from './Login';
 import LoginApi from "./api/LoginApi";
 import AddTransaction from "./AddTransaction";
 
 class App extends React.Component {
     state = {response: "",addresponse:""};
-    LoggingIn = async (user, pass) => {
-        const loggedin = await LoginApi.post("/login",{
-            username: user,
-            password: pass
-        });
-        this.setState({response: loggedin});
-        console.log(loggedin);
-    }
+    // LoggingIn = async (user, pass) => {
+    //     const loggedin = await LoginApi.post("/login",{
+    //         username: user,
+    //         password: pass
+    //     });
+    //     this.setState({response: loggedin});
+    //     console.log(loggedin);
+    // }
     // Belongs to AddTransaction Component
     Transacting = async(accKey, order, amount) => {
         const transacted = await LoginApi.post("/transactions/add",{
@@ -27,7 +27,7 @@ class App extends React.Component {
     render() {
         return (
             <div>
-                <Login onsubmit={this.LoggingIn} />
+                {/* <Login onsubmit={this.LoggingIn} /> */}
             
                 <AddTransaction ontransact={this.Transacting} />
             </div>
