50e74295f00e4362360905f905fba107ab0cd3d6
cyclonekid8 <muzafarshahmarican@gmail.com>
2021-03-05 11:56:02 +0800

    Nav bar and initial routing

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -2,7 +2,39 @@ import React from 'react';
 import {withRouter} from 'react-router-dom';
 
 class Nav extends React.Component {
+    
+    state={ clickedon : false};
+    clicked = () => {
+        if(this.state.clickedon===false) {
+            this.setState({clickedon:true});
+        }
+        else {
+            this.setState({clickedon:false});
+        }
+    }
     render() {
-        return (<div>Nav</div>);
+        return (
+        <div>
+            <li>
+                User details
+                
+            </li>
+            <li>
+                View wallet balance
+            </li>
+            <li>
+                Current pricing of assets
+            </li>
+            <li>
+                Historical pricing of assets
+            </li>
+            <li>
+                View past transactions
+            </li>
+            <li>
+               Buy and sell assets
+            </li>
+        </div>);
     }
 }
+export default withRouter(Nav);
--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router} from 'react-router-dom';
 import App from './App';
 
-ReactDOM.render(<App/>,document.querySelector("#root"));
+ReactDOM.render(<Router><App/></Router>,document.querySelector("#root"));
 
 
 
