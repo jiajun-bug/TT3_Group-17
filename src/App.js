386b1abb8f8a677cb360e5c8eff079ccfa8807e1
Hariz1995 <harizyousuf@gmail.com>
2021-03-05 12:22:57 +0800

    Login Page Created

--- src/App.js
+++ src/App.js
@@ -1,13 +1,27 @@
 import React from 'react';
-import { render } from 'react-dom';
+import Login from './Login';
+import LoginApi from "./api/LoginApi";
 
 class App extends React.Component {
+    state = {response: ""};
+    LoggingIn = async (user, pass) => {
+        const loggedin = await LoginApi.post("/login",{
+            username: user,
+            password: pass
+        });
+        this.setState({response: loggedin});
+        console.log(loggedin);
+    }
+
     render() {
         return (
-            <div>Hello</div>
+            <div>
+                <Login onsubmit={this.LoggingIn} />
+            </div>
+            
         );
 
-    }
+    };
 }
 
 export default App;
