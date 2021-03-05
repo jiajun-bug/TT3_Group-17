c83dc937c3d05761789f42b54bad461cb6f9d8e8
cyclonekid8 <muzafarshahmarican@gmail.com>
2021-03-05 12:07:07 +0800

    imported routers

--- src/App.js
+++ src/App.js
@@ -1,10 +1,33 @@
 import React from 'react';
-import { render } from 'react-dom';
+import { Route, Switch, withRouter } from "react-router-dom";
+import Nav from './components/Nav';
 
 class App extends React.Component {
     render() {
         return (
-            <div>Hello</div>
+            <div>
+                <Switch>
+                    <Route path="/view">
+                        <Nav/>
+                        <View />
+                    </Route>
+                    <Route path="/balance">
+                        <Nav/>
+                    </Route>
+                    <Route path="/price">
+                        <Nav/>
+                    </Route>
+                    <Route path="/historical">
+                        <Nav/>
+                    </Route>
+                    <Route path="/pasttransaction">
+                        <Nav/>
+                    </Route>
+                    <Route path="/buyandsell">
+                        <Nav/>
+                    </Route>
+                </Switch>
+            </div>
         );
 
     }
