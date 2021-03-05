a3241800df1a76ea2fefdebeac999285aa04acba
cyclonekid8 <muzafarshahmarican@gmail.com>
2021-03-05 11:39:10 +0800

    initial commit

---
+++ src/components/Nav.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import {withRouter} from 'react-router-dom';
+
+class Nav extends React.Component {
+    render() {
+        return (<div>Nav</div>);
+    }
+}
---
+++ src/components/View.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { withRouter } from 'react-router-dom';
+
+class View extends React.Component {
+    
+    
+    
+    
+    render() {
+        return (
+            <div>View user details</div>
+        );
+    }
+}
